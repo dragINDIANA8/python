4e4817cc8ae92b91e7e7fe52e2eea8ddbc6c2007
Snehatiwarideepti <151862173+Snehatiwarideepti@users.noreply.github.com>
2023-12-06 23:35:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+a=int(input("Enter the value 1:"))
+b=int(input("Enter the value 2:"))
+c=input("Enter the Opr..:")
+if c=="+":
+    print("Addition is",a+b)
+elif c=="-":
+    print("Subtraction is",a-b)
+elif c=="*":
+    print("Multiplication is",a*b)
+elif c=="/":
+    print("Division is",a/b)
+else:
+    print("Not Valid")
