af472ee3a6e5ec532365b26c5e8bbe8a74a5bda5
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2024-07-09 13:08:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ a=int(input("Enter the value 1:"))
 b=int(input("Enter the value 2:"))
 c=input("Enter the Opr..:")
 if c=="+":
-    print("Addition is",a+b)
+    print("Addition of", a ,"+",b ,"is" , a+b)
 elif c=="-":
-    print("Subtraction is",a-b)
+    print("Substraction of", a ,"+",b ,"is" , a+b)
 elif c=="*":
-    print("Multiplication is",a*b)
+    print("Multiplication of", a ,"+",b ,"is" , a+b)
 elif c=="/":
-    print("Division is",a/b)
+    print("Division of", a ,"+",b ,"is" , a+b)
 else:
     print("Not Valid")
